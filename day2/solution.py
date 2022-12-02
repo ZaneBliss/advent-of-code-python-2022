01d21ea6b5f7ce198c1dad8867acbb1b4ba6e3a4
Zane Bliss <zane.bliss@daveramsey.com>
2022-12-02 13:28:46 -0600

    day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -14,6 +14,20 @@ def part_1(file):
     return total_score
 
 
+def part_2(file):
+    total_score = 0
+
+    with open(file) as f:
+        for line in f:
+            moves = list(line.strip())
+            opponent_move = map_move_to_dict(moves[0])
+            outcome = map_outcome_to_dict(moves[-1])
+
+            total_score += calculate_score_for_outcome(opponent_move, outcome)
+
+    return total_score
+
+
 def calculate_score(opponent_move, _self_move):
     move_score = _self_move['score']
     round_score = 0
@@ -41,6 +55,39 @@ def calculate_score(opponent_move, _self_move):
             round_score = 3
 
     return move_score + round_score
+
+
+def calculate_score_for_outcome(opponent_move, outcome):
+    round_score = outcome['score']
+    move_score = 0
+
+    if outcome['status'] == 'loss':
+        if opponent_move['action'] == 'rock':
+            move_score = 3
+        if opponent_move['action'] == 'paper':
+            move_score = 1
+        if opponent_move['action'] == 'scissors':
+            move_score = 2
+    if outcome['status'] == 'tie':
+        move_score = opponent_move['score']
+    if outcome['status'] == 'win':
+        if opponent_move['action'] == 'rock':
+            move_score = 2
+        if opponent_move['action'] == 'paper':
+            move_score = 3
+        if opponent_move['action'] == 'scissors':
+            move_score = 1
+
+    return move_score + round_score
+
+
+def map_outcome_to_dict(outcome):
+    if outcome == 'X':
+        return {'status': 'loss', 'score': 0}
+    elif outcome == 'Y':
+        return {'status': 'tie', 'score': 3}
+    else:
+        return {'status': 'win', 'score': 6}
 
 
 def map_move_to_dict(move):
--- day2/test_solution.py
+++ day2/test_solution.py
@@ -10,3 +10,9 @@ class TestSolution(unittest.TestCase):
     def test_part1_actual(self):
         self.assertEqual(11063, solution.part_1('input.txt'))
 
+    def test_part2_sample(self):
+        self.assertEqual(12, solution.part_2('sample.txt'))
+
+    def test_part2_actual(self):
+        self.assertEqual(10349, solution.part_2('input.txt'))
+
